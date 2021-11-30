579318e09155077b9d09f1ec7b7b43a88b410a25
madan <madandev@excalibur.local>
2021-11-30 20:39:43 +0530

    changed url

--- src/util/request.js
+++ src/util/request.js
@@ -2,6 +2,6 @@ import Axios from "axios";
 
 const req = Axios.create({
   // baseURL: " http://localhost:3004/api",
-  baseURL: "https://fernway-travel.herokuapp.com/",
+  baseURL: "https://fernway-travel.herokuapp.com/api",
 });
 export default req;
